1aa5af12d2bdd12b24690f6930645f0bdaeb3f25
Denys <dkononov.ua@gmail.com>
2023-02-14 11:24:29 +0200

    add-components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaymentsComponent } from './payments/payments.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'registration' , component: RegistrationComponent},
+  {path: '' , redirectTo: 'home' , pathMatch: 'full' },
+  {path: 'payments' , component: PaymentsComponent},
+  {path: 'services' , component: ServicesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
