33657e035cf8eca0734b618faa6335f29d569e58
Denys <dkononov.ua@gmail.com>
2023-02-16 23:01:08 +0200

    reg-form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ import { WaterComponent } from './services/water/water.component';
 
 const routes: Routes = [
   {path: 'registration' , component: RegistrationComponent},
-  {path: '' , redirectTo: 'home' , pathMatch: 'full' },
+  {path: '' , redirectTo: 'payments' , pathMatch: 'full' },
   {path: 'payments' , component: PaymentsComponent},
   {path: 'services' , component: ServicesComponent},
   {path: 'lessee' , component: LesseeComponent},
