28c747002175938b2f27985535d4b9d4cdda2832
Denys <dkononov.ua@gmail.com>
2023-02-18 17:33:39 +0200

    registrnew

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
-export class RegistrationComponent {
 
+export class RegistrationComponent {
 }
+
+
+
