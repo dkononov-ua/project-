19bddba0ddd823e7264d043817de0df925038a0f
Denys <dkononov.ua@gmail.com>
2023-02-19 10:25:19 +0200

    registerend

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -5,9 +5,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
+export class RegistrationComponent implements OnInit {
 
-export class RegistrationComponent {
+  constructor() { }
+
+  ngOnInit(): void {
+    const registerButton = document.getElementById('register');
+    const loginButton = document.getElementById('login');
+    const container = document.getElementById('container');
+
+    registerButton?.addEventListener("click", () => {
+      container?.classList.add("right-panel-active");
+    });
+
+    loginButton?.addEventListener("click", () => {
+      container?.classList.remove("right-panel-active");
+    });
+  }
 }
+
 
 
 
