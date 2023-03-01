bbf454bbe18804777a817086dd27b4206ee1b090
Denys <dkononov.ua@gmail.com>
2023-03-01 17:08:46 +0200

    fix1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -19,25 +19,3 @@ export const environment = {
   }
 };
 
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDO4kjHR-s-tMrYzXka-oEuJ-Y0G2Z9mHo",
-  authDomain: "konnect-kononov.firebaseapp.com",
-  projectId: "konnect-kononov",
-  storageBucket: "konnect-kononov.appspot.com",
-  messagingSenderId: "418242145493",
-  appId: "1:418242145493:web:01a06d68e485b8a2a4865c",
-  measurementId: "G-QW6WJ0YSR0"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
