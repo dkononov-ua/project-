66f6204a02a4d8dac297d559821d8d13da4ec380
Denys <dkononov.ua@gmail.com>
2023-05-10 14:07:01 +0300

    search4

--- src/app/components/search-term/search-term.component.ts
+++ src/app/components/search-term/search-term.component.ts
@@ -1,8 +1,32 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Subscription, debounceTime, filter } from 'rxjs';
 import { FormGroup, FormControl } from '@angular/forms';
 import { SendDataService } from 'src/app/services/send-data.service';
+import { Router } from '@angular/router';
+
+interface SearchParams {
+  [key: string]: any;
+}
+
+interface SearchParams {
+  country: string;
+  region: string;
+  city: string;
+  rooms: number | string;
+  repair_status: string;
+  area: string;
+  selectedKitchen_area: string;
+  balcony: string;
+  bunker: string;
+  animals: string;
+  distance_metro: string;
+  distance_stop: string;
+  distance_green: string;
+  distance_shop: string;
+  distance_parking: string;
+  limit: string;
+}
 
 @Component({
   selector: 'app-search-term',
@@ -10,50 +34,192 @@ import { SendDataService } from 'src/app/services/send-data.service';
   styleUrls: ['./search-term.component.scss'],
 })
 export class SearchTermComponent implements OnInit {
+  searchQueryString = '';
+  searchParamsString: string = '';
+
+  private subscription: Subscription | undefined;
 
   selectedCity!: string;
-  selectedRooms!: number;
+  selectedRooms!: number | any;
   selectedRating!: number;
-  selectedArea!: number;
+  selectedArea!: string;
+  selectedKitchen_area!: string;
   selectedRegion!: string;
-
+  selectedAnimals!: string;
+  selectedDistance_metro!: string;
+  selectedDistance_stop!: string;
+  selectedDistance_green!: string;
+  selectedDistance_shop!: string;
+  selectedDistance_parking!: string;
+  selectedBunker!: string;
+  selectedBalcony!: string;
+  flats: any[] | undefined;
 
   myForm: FormGroup | undefined | any;
   sendData: any;
   selectedRepair_status: any;
-  selectedDistance_parking: any;
-  selectedDistance_green: any;
-  selectedDistance_shop: any;
-  selectedDistance_stop: any;
-  selectedDistance_metro: any;
-  selectedAnimals: any;
-
-  onSubmit() {
-    const formValue = this.myForm?.value;
-    const params = new HttpParams()
-      .set('price_of', formValue.price_of)
-      .set('price_to', formValue.price_to)
-      .set('students', formValue.students)
-      .set('woman', formValue.woman)
-      .set('man', formValue.man)
-      .set('family', formValue.family);
-    this.http.get(`${this.endpoint}`, { params }).subscribe(data => {
-    });
-  }
-
-  onSubmit2() {
-    const formData = new FormData();
-    formData.append('region', this.selectedRegion),
-    this.sendData.sendFormData(formData).subscribe((response: any) => {
-      console.log('Server response:', response);
-    });
-  }
-
 
   endpoint = 'http://localhost:3000/search/flat';
   private searchSubscription: Subscription | null = null;
+  form: any;
+  searchParamsArr: any;
 
-  constructor(private http: HttpClient, private SendDataService: SendDataService) { }
+  constructor(private http: HttpClient, private SendDataService: SendDataService, private router: Router) { }
+
+  ngOnInit() {
+    this.myForm = new FormGroup({
+      price_of: new FormControl(),
+      price_to: new FormControl(),
+      students: new FormControl(),
+      woman: new FormControl(),
+      man: new FormControl(),
+      family: new FormControl()
+    });
+
+    this.myForm.valueChanges
+      .pipe(
+        debounceTime(2000)
+      )
+      .subscribe(() => {
+        this.onSubmit();
+      });
+  }
+
+  onSubmit() {
+    const params: SearchParams = {
+      region: this.selectedRegion || '',
+      city: this.selectedCity || '',
+      rooms: this.selectedRooms || '',
+      area: this.selectedArea || '',
+      repair_status: this.selectedRepair_status || '',
+      selectedKitchen_area: this.selectedKitchen_area || '',
+      balcony: this.selectedBalcony || '',
+      bunker: this.selectedBunker || '',
+      animals: this.selectedAnimals || '',
+      distance_metro: this.selectedDistance_metro || '',
+      distance_stop: this.selectedDistance_stop || '',
+      distance_green: this.selectedDistance_green || '',
+      distance_shop: this.selectedDistance_shop || '',
+      distance_parking: this.selectedDistance_parking || '',
+      limit: '',
+      country: '',
+    };
+
+    let searchParamsArr: string[] = [];
+
+    this.addSelectedValue(searchParamsArr, 'Регіон', this.selectedRegion);
+    this.addSelectedValue(searchParamsArr, 'Місто', this.selectedCity);
+    this.addSelectedValue(searchParamsArr, 'Кімнати', this.selectedRooms);
+    this.addSelectedValue(searchParamsArr, 'Площа', this.selectedArea);
+    this.addSelectedValue(searchParamsArr, 'Ремонт', this.selectedRepair_status);
+    this.addSelectedValue(searchParamsArr, 'Кухня', this.selectedKitchen_area);
+    this.addSelectedValue(searchParamsArr, 'Балкон', this.selectedBalcony);
+    this.addSelectedValue(searchParamsArr, 'Укриття', this.selectedBunker);
+    this.addSelectedValue(searchParamsArr, 'Тварини', this.selectedAnimals);
+    this.addSelectedValue(searchParamsArr, 'Метро', this.selectedDistance_metro);
+    this.addSelectedValue(searchParamsArr, 'Зупинки', this.selectedDistance_stop);
+    this.addSelectedValue(searchParamsArr, 'Парк', this.selectedDistance_green);
+    this.addSelectedValue(searchParamsArr, 'Маркет', this.selectedDistance_shop);
+    this.addSelectedValue(searchParamsArr, 'Парковка', this.selectedDistance_parking);
+
+    this.searchParamsString = searchParamsArr.join(', ');
+    console.log('Обрані параметри пошуку:', this.searchParamsString);
+
+    const url = this.buildSearchURL(params);
+
+    this.updateURL();
+    this.fetchFlatData(url);
+  }
+
+  // Додавання значення до масиву параметрів пошуку
+  addSelectedValue(searchParamsArr: string[], label: string, value: string) {
+    if (value) {
+      searchParamsArr.push(`${label}: ${value}`);
+    }
+  }
+
+  // Формування URL для пошуку
+  buildSearchURL(params: SearchParams): string {
+    const endpoint = 'http://localhost:3000/search/flat';
+    const paramsString = Object.keys(params)
+      .filter(key => params[key] !== '') // Видаляємо пусті значення
+      .map(key => key + '=' + params[key])
+      .join('&');
+    return `${endpoint}?${paramsString}`;
+  }
+
+  // Оновлення URL з використанням значень пошуку
+  updateURL() {
+    this.router.navigate([], {
+      queryParams: { searchParams: this.searchParamsString },
+      queryParamsHandling: 'merge'
+    });
+  }
+
+  fetchFlatData(url: string) {
+    this.subscription = this.http.get<{ flat_inf: any[] }>(url).subscribe((data) => {
+      const flatInfo = data.flat_inf; // Отримуємо інформацію про оселі з відповіді
+      if (flatInfo) {
+        // console.log(flatInfo); // Виводимо всі оселі
+
+        // Додаткова фільтрація на сервері
+        const filteredFlats = this.filterFlatsBySelections(flatInfo);
+
+        console.log(filteredFlats); // Виводимо відфільтровані дані в консоль
+      } else {
+        console.log('flatInfo is undefined');
+      }
+    });
+  }
+
+  filterFlatsBySelections(flatInfo: any[]): any[] {
+    const filteredFlats = flatInfo.filter((flat) => {
+      // Перевіряємо кожну оселю на відповідність вибраним значенням у селектах
+      if (this.selectedCity && flat.city !== this.selectedCity) {
+        return false; // Оселя не відповідає вибраному місту
+      }
+      if (this.selectedRooms && flat.rooms !== this.selectedRooms) {
+        return false; // Оселя не відповідає вибраній кількості кімнат
+      }
+      if (this.selectedArea) {
+        let areaLabel = '';
+        let areaFilter = false;
+        switch (this.selectedArea) {
+          // ...
+        }
+        if (areaFilter) {
+          this.searchParamsArr.push(`Площа: ${areaLabel}`);
+        }
+        return areaFilter;
+      }
+      if (this.myForm) {
+        const price_of = this.myForm.get('price_of').value;
+        const price_to = this.myForm.get('price_to').value;
+        if (price_of && flat.price < price_of) {
+          return false; // Оселя не відповідає вибраній мінімальній ціні
+        }
+        if (price_to && flat.price > price_to) {
+          return false; // Оселя не відповідає вибраній максимальній ціні
+        }
+        const students = this.myForm.get('students').value;
+        const woman = this.myForm.get('woman').value;
+        const man = this.myForm.get('man').value;
+        const family = this.myForm.get('family').value;
+        if ((students && flat.target !== 'students') ||
+            (woman && flat.target !== 'woman') ||
+            (man && flat.target !== 'man') ||
+            (family && flat.target !== 'family')) {
+          return false; // Оселя не відповідає вибраній категорії пошуку
+        }
+      }
+      return true; // Оселя відповідає всім вибраним значенням у селектах та формі
+    });
+
+    console.log(filteredFlats); // Виводимо відфільтровані дані в консоль
+
+    return filteredFlats;
+  }
+
 
   search(searchString: string) {
     this.searchSubscription?.unsubscribe();
@@ -65,67 +231,15 @@ export class SearchTermComponent implements OnInit {
     }
   }
 
-  // submitForm() {
-  //   this.SendDataService.sendFormData(this.myForm).subscribe(
-  //     response => {
-  //       console.log('Дані успішно відправлені на сервер');
-  //     },
-  //     error => {
-  //       console.error('Сталася помилка при відправленні даних на сервер', error);
-  //     }
-  //   );
-  // }
-
-  debounce(cb: any, ms: number) {
-    let timer: any = null;
-    return () => {
-      if (timer) {
-        clearTimeout(timer);
-        timer = null;
-      }
-      timer = setTimeout(cb, ms);
-    };
-  }
-
   inputSearchHandler(event: any) {
-    this.debounce(() => {
-      const searchString = event.target.value.trim();
+    const searchString = event.target.value.trim();
+    setTimeout(() => {
       this.search(searchString);
-    }, 3000)();
-  }
-
-  ngOnInit() {
-    this.myForm = new FormGroup({
-      price_of: new FormControl(),
-      price_to: new FormControl(),
-      students: new FormControl(),
-      woman: new FormControl(),
-      man: new FormControl(),
-      family: new FormControl()
-    });
-    this.onPriceRangeChange();
-
-    const inputSearch = document.querySelector('.search-input') as HTMLInputElement;
-    inputSearch.addEventListener('keyup', (event) => {
-      this.inputSearchHandler(event);
-    });
-
+    }, 3000);
   }
 
   minValue: number = 0;
   maxValue: number = 100000;
-
-  selectedMinValue: number = 0;
-  selectedMaxValue: number = 100000;
-
-  onPriceRangeChange() {
-    if (this.selectedMaxValue < this.selectedMinValue) {
-      const temp = this.selectedMinValue;
-      this.selectedMinValue = this.selectedMaxValue;
-      this.selectedMaxValue = temp;
-    }
-  }
-
 
   regions = [
     { id: 0, name: 'Всі області' },
@@ -157,7 +271,6 @@ export class SearchTermComponent implements OnInit {
   ];
 
   cities = [
-    { id: 0, name: 'Всі області' },
     { id: 1, name: 'Вінниця' },
     { id: 2, name: 'Луцьк' },
     { id: 3, name: 'Дніпро' },
@@ -182,6 +295,7 @@ export class SearchTermComponent implements OnInit {
     { id: 22, name: 'Черкаси' },
     { id: 23, name: 'Чернівці' },
     { id: 24, name: 'Чернігів' },
-    { id: 25, name: 'АР Крим' }
+    { id: 25, name: 'АР Крим' },
+    { id: 26, name: 'Крюківщина' },
   ];
 }
