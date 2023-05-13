918684665d4f8c8377a24366680eda63e9776983
Denys <dkononov.ua@gmail.com>
2023-05-13 22:19:48 +0300

    searchfix

--- src/app/search/housing-search/housing-search.component.ts
+++ src/app/search/housing-search/housing-search.component.ts
@@ -1,9 +1,9 @@
 import { animate, style, transition, trigger } from '@angular/animations';
 import { HttpClient } from '@angular/common/http';
-import { Component, Input, Output, EventEmitter, HostBinding, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { FilterService } from './filter.service';
 
 interface FlatInfo {
@@ -42,12 +42,7 @@ interface Flat {
 interface FlatInfo {
   flat_id: number;
   flat_img: string;
-  // інші поля
 }
-
-type FlatPhoto = string;
-
-
 
 @Component({
   selector: 'app-housing-search',
@@ -62,51 +57,38 @@ type FlatPhoto = string;
       transition('* => void', [
         animate('1000ms ease-in-out', style({ transform: 'translateX(-100%)' }))
       ])
-    ])
+    ]),
+    trigger('slideAnimation', [
+      transition(':enter', [
+        style({ transform: 'translateY(100%)', opacity: 0 }),
+        animate('300ms', style({ transform: 'translateY(0)', opacity: 1 }))
+      ])
+    ]),
   ]
 })
-
-
 
 export class HousingSearchComponent implements OnInit {
 
   private filterSubscription: Subscription | undefined;
-  subscription: Subscription | undefined;
   flatInfo: FlatInfo[] = [];
   filteredFlats: FlatInfo[] | undefined;
   selectedFlat: FlatInfo | any;
   filterForm: FormGroup;
-  currentCard: any;
-  images: string[] = [];
   flatImages: any[] = [];
   selectedFlatPhotos: string[] = [];
-  flatPhotos: string[] = [];
-  carouselImages!: { flat_id: any; img: string; }[]
-  selectedFlatPhotoUrls: string[] = [];
   currentFlatPhotos: string[] = [];
   currentPhotoIndex: number = 0;
+  isCarouselAnimating!: boolean;
+  limit: number = 10;
+  additionalLoadLimit: number = 5;
+  offset: number = 0;
 
-
-
-  carouselOptions = {
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    dots: true,
-    arrows: false,
-    infinite: true
-  };
-
-  flatInfoWithImages: { flat_img: FlatImage; region: string; city: string; rooms: string; area: string; repair_status: string; selectedKitchen_area: string; balcony: string; bunker: string; animals: string; distance_metro: string; distance_stop: string; distance_green: string; distance_shop: string; distance_parking: string; limit: string; country: string; price: any; id: number; name: string; photos: string[]; img: string; }[] | undefined;
-  selectedFlatImages: any;
 
   constructor(
     private filterService: FilterService,
     private formBuilder: FormBuilder,
     private http: HttpClient,
     private router: Router,
-    private changeDetector: ChangeDetectorRef,
   ) {
     this.filterForm = this.formBuilder.group({});
   }
@@ -140,7 +122,7 @@ export class HousingSearchComponent implements OnInit {
   }
 
   getDefaultImage(photo: string | undefined | null): string {
-    if (photo === undefined || photo === null) {
+    if (!photo) {
       return 'http://localhost:3000/img/flat/housing_default.svg';
     } else {
       return this.getImageUrl(photo);
@@ -149,23 +131,19 @@ export class HousingSearchComponent implements OnInit {
 
   fetchFlatData(url: string) {
     this.http.get<{ flat_inf: FlatInfo[], flat_img: any[] }>(url).subscribe((data) => {
-      const flatInfo = data.flat_inf;
-      const flatImages = data.flat_img;
-
-      if (flatInfo && flatImages) {
-        this.flatInfo = flatInfo;
-        this.flatImages = flatImages;
+      const { flat_inf, flat_img } = data;
+      if (flat_inf && flat_img) {
+        this.flatInfo = flat_inf;
+        this.flatImages = flat_img;
         this.selectedFlat = this.flatInfo[0];
-        this.updateSelectedFlatPhotos(); // Оновити фотографії обраної оселі
+        this.updateSelectedFlatPhotos();
       }
     });
   }
 
-
   updateSelectedFlatPhotos() {
     const selectedFlatId = this.selectedFlat?.flat_id;
     const selectedFlatImages = this.flatImages.find(flatImage => flatImage.flat_id === selectedFlatId)?.img || [];
-
     this.selectedFlatPhotos = selectedFlatImages;
   }
 
@@ -179,23 +157,20 @@ export class HousingSearchComponent implements OnInit {
   }
 
   selectFlat(flat: FlatInfo) {
+    const selectedFlatId = this.selectedFlat?.flat_id;
+    const selectedFlatImages = this.flatImages.find(flatImage => flatImage.flat_id === selectedFlatId)?.img || [];
+    this.selectedFlatPhotos = selectedFlatImages;
     this.selectedFlat = flat;
     this.updateSelectedFlatPhotos();
     this.currentFlatPhotos = [...this.selectedFlatPhotos];
-    this.updateCurrentPhotoIndex(0); // Скидати поточний індекс при виборі нової оселі
+    this.updateCurrentPhotoIndex(0);
   }
-
-
-
-
-
 
   updateFilteredFlats() {
     if (this.filteredFlats) {
       this.filteredFlats = [...this.filteredFlats]; // Створити копію списку обраних осель
     }
   }
-
 
   updateFilteredData(filterValue: any) {
     this.filterForm.patchValue(filterValue);
@@ -210,18 +185,18 @@ export class HousingSearchComponent implements OnInit {
   getFlatImageUrl(flat: FlatInfo): any {
     let imageUrl = '';
 
-    this.flatImages.forEach((e: any) => {
-      if (e.flat_id === flat.flat_id) {
-        if (e.img[0] === undefined === null) {
-          imageUrl = 'http://localhost:3000/housing_default.svg';
-        } else {
-          imageUrl = this.getImageUrl(e.img[0]);
-        }
+    const flatImage = this.flatImages.find(flatImage => flatImage.flat_id === flat.flat_id);
+    if (flatImage) {
+      if (!flatImage.img[0] === undefined === null) {
+        imageUrl = 'http://localhost:3000/housing_default.svg';
+      } else {
+        imageUrl = this.getImageUrl(flatImage.img[0]);
       }
-    });
+    }
 
     return imageUrl;
   }
+
 
   getImageUrl(fileName: string | string[]): string {
     if (typeof fileName === 'string') {
@@ -235,16 +210,49 @@ export class HousingSearchComponent implements OnInit {
   currentCardIndex: number = 0;
   cards: FlatInfo[] = [];
 
+  onPrevCard() {
+    if (!this.isCarouselAnimating) {
+      this.isCarouselAnimating = true;
+      this.currentCardIndex = this.calculateCardIndex(this.currentCardIndex - 1);
+      this.updateSelectedFlat();
+      this.updateCurrentPhotoIndex(0);
+      this.currentFlatPhotos = [...this.selectedFlatPhotos];
+
+      setTimeout(() => {
+        this.isCarouselAnimating = false;
+      }, 100);
+    }
+  }
+
   onNextCard() {
-    this.currentCardIndex = (this.currentCardIndex + 1) % this.filteredFlats!.length;
+    if (!this.isCarouselAnimating) {
+      this.isCarouselAnimating = true;
+      this.currentCardIndex = this.calculateCardIndex(this.currentCardIndex + 1);
+      this.updateSelectedFlat();
+      this.updateCurrentPhotoIndex(0);
+      this.currentFlatPhotos = [...this.selectedFlatPhotos];
+
+      setTimeout(() => {
+        this.isCarouselAnimating = false;
+      }, 100);
+    }
+  }
+
+  private calculateCardIndex(index: number): number {
+    const length = this.filteredFlats?.length || 0;
+    return (index + length) % length;
+  }
+
+  private updateSelectedFlat() {
     this.selectedFlat = this.filteredFlats![this.currentCardIndex];
     this.updateSelectedFlatPhotos();
   }
 
-  onPrevCard() {
-    this.currentCardIndex = (this.currentCardIndex - 1 + this.filteredFlats!.length) % this.filteredFlats!.length;
-    this.selectedFlat = this.filteredFlats![this.currentCardIndex];
-    this.updateSelectedFlatPhotos();
+  loadMore() {
+    const additionalFlats = this.flatInfo.slice(this.limit + this.offset, this.limit + this.offset + this.additionalLoadLimit);
+    this.filteredFlats = [...this.filteredFlats!, ...additionalFlats];
+    this.offset += this.additionalLoadLimit;
   }
+
 
 }
